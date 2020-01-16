737f54e59f78f774401df697461f32eb5da225c0
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-15 20:51:53 -0800

    dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import useDarkMode from '../hooks/useDarkMode';
+import React, { useState } from 'react';
+import { Link, NavLink } from "react-router-dom";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const Navbar = () => {
+import useDarkMode from "../hooks/useDarkMode";
+
+const Navbar = ({ coinData }) => {
   const [darkMode, setDarkMode] = useDarkMode();
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => {
+    setDropdownOpen(!dropdownOpen);
+  }
+
   return (
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>
+      <Link to="/" className="title"><h1 className="title">Crypto Tracker</h1></Link>
+
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
+
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>Coin</DropdownToggle>
+        <DropdownMenu right>
+
+          {coinData.map(coin => (
+            <DropdownItem ><NavLink to={`/${coin.name}`}>{coin.name}</NavLink></DropdownItem>
+          ))}
+
+        </DropdownMenu>
+      </Dropdown>
+
     </nav>
   );
 };
